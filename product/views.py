e61a209ebb13f0b6130a648512681ea3465906e8
damin0320 <damin0320@gmail.com>
2021-03-19 18:41:55 +0900

    views.py Setting

--- product/views.py
+++ product/views.py
@@ -3,14 +3,20 @@ import json
 from django.views import View
 from django.http  import JsonResponse
 
-from .models import Category, SubCategory, Product, DiscountRate, Option, ProductOption
+from .models     import Category, SubCategory, Product, ProductImage, ProductDescription, BookDescription, DiscountRate, Option, ProductOption, Review, ProductInquiry
+from user.models import User
 
 class ProductShowInformationView(View):
     def get(self, request):
         productshowinformations = Product.objects.all()
+        productimages           = Product.productimage.all()
+        productdescriptions     = Product.productdescription.all()
+        bookdescriptions        = Product.bookdescription.all()
         options                 = Product.product_options.filter(subcategory=data['subcategory'])
         discount_rates          = Product.discountrate.filter(product=data['product'])
         productoptions          = Product.product_options.filter(stock=data['stock'], additional_price=data['additional_price'])
+        reviews                 = Product.review.all()
+        product_inquiry         = Product.product_inquiry.all()
 
         result = []
         
@@ -20,6 +26,29 @@ class ProductShowInformationView(View):
                 'price'              : productshowinformation.price,
                 'thumbnail_image_url': productshowinformation.thumbnail_image_url
             }
+        for productimage in productimages:
+            productimage_dict = {
+                'image_url' : image_url,
+                'product' : productshowinformations
+            }    
+        for productdescription in productdescriptions:
+            prductdescription_dict = {
+                'name' : name,
+                'material' : material,
+                'size_cm' : size_cm,
+                'manufacture_country' : manufacture_country,
+                'caution' : caution,
+                'product' : productshowinformations
+            }
+        for bookdescription in bookdescriptions:
+            bookdescription_dict = {
+                'title' : title,
+                'publisher' : publisher,
+                'size_mm' : size_mm,
+                'total_page' : total_page,
+                'product' : productshowinformations
+            }
+            
         for option in options:
             option_dict = {
                 'classification': classification,
@@ -32,7 +61,14 @@ class ProductShowInformationView(View):
             }
         for discount_rate in discount_rates:
             discount_rate_dict = {
-                'rate' : rate
+                'rate' : discount_rate.rate
+            }
+        for review in reviews:
+            review_dict = {
+                'content' : review.content,
+                'product' : review.productshowinformations,
+                'user'    : review.user.user
+                
             }
         result.append(productshowinformation_dict, option_dict, discount_rate_dict)
         return JsonResponse({'result' : result}, status=200)
