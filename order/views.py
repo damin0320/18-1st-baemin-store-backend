da2054ea04f04e5b2ba1158bb46c54bc8c6488e2
taekhyang <xorgid1195@gmail.com>
2021-03-22 21:34:37 +0900

    FIX : fixed get_or_create() IntegretyError

--- order/views.py
+++ order/views.py
@@ -15,6 +15,7 @@ USER_ID = 2
 
 
 class CartView(View):
+    @transaction.atomic
     def post(self, request):
         try:
             data = json.loads(request.body)
@@ -24,25 +25,30 @@ class CartView(View):
             quantity   = data['quantity'] if not data['quantity'] == 0 else None
             options    = data['options']
 
-            for option in options:
-                product_option_id       = option['product_option_id']
-                product_option_quantity = option['product_option_quantity']
-
-            before_purchase, _      = OrderStatus.objects.get_or_create(id=1, status='구매전')
-            order, is_order_created = Order.objects.get_or_create(user_id=user_id, order_status=before_purchase)
-
+            before_purchase, _ = OrderStatus.objects.get_or_create(id=1, status='구매전')
+            order, _           = Order.objects.get_or_create(user_id=user_id, order_status=before_purchase)
             if options:
                 for option in options:
-                    cart, _ = Cart.objects.get_or_create(
-                                                         quantity          = option['product_option_quantity'],
-                                                         product_option_id = option['product_option_id'],
-                                                         order             = order,
-                                                         product_id        = product_id
-                                                    )
-###### 옵션 quantity 별로 추가
+                    cart, is_created = Cart.objects.get_or_create(
+                                                                  product_id        = product_id,
+                                                                  order             = order,                                                                  
+                                                                  product_option_id = option['product_option_id'],
+                                                                  defaults={'quantity': option['product_option_quantity']}
+                                                                )
+                    if not is_created:
+                        cart.quantity += option['product_option_quantity']
+                        cart.save()
 
-            if not is_order_created:
+                return JsonResponse({'message': 'SUCCESS'}, status=201)
+
+            cart, is_created = Cart.objects.get_or_create(
+                                                            product_id = product_id,
+                                                            order      = order,
+                                                            defaults={'quantity': quantity}
+                                                        )
+            if not is_created:
                 cart.quantity += quantity
+            return JsonResponse({'message': 'SUCCESS'}, status=201)
 
         except JSONDecodeError:
             return JsonResponse({'message': 'JSON_DECODE_ERROR'}, status=400)
