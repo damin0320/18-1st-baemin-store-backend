9a03da058d320eec7c06fa2f10ae01e7f48b5d3b
taekhyang <xorgid1195@gmail.com>
2021-03-22 19:54:08 +0900

    MODIFY : cleaned up codes

--- order/views.py
+++ order/views.py
@@ -3,6 +3,7 @@ from json import JSONDecodeError
 
 from django.views import View
 from django.http  import JsonResponse
+from django.db    import transaction
 
 from .models        import Order, OrderStatus, Cart
 from product.models import Product, ProductOption
@@ -27,18 +28,20 @@ class CartView(View):
                 product_option_id       = option['product_option_id']
                 product_option_quantity = option['product_option_quantity']
 
-            before_purchase, is_created = OrderStatus.objects.get_or_create(id=1, status='구매전')
+            before_purchase, _      = OrderStatus.objects.get_or_create(id=1, status='구매전')
+            order, is_order_created = Order.objects.get_or_create(user_id=user_id, order_status=before_purchase)
 
-            order = Order.objects.create(user_id=user_id, order_status=before_purchase)
+            if options:
+                for option in options:
+                    cart, _ = Cart.objects.get_or_create(
+                                                         quantity          = option['product_option_quantity'],
+                                                         product_option_id = option['product_option_id'],
+                                                         order             = order,
+                                                         product_id        = product_id
+                                                    )
+###### 옵션 quantity 별로 추가
 
-            # product_option_id 없을 때 처리
-            cart, is_created = Cart.objects.get_or_create(
-                                                          quantity          = quantity,
-                                                          order             = order,
-                                                          product_id        = product_id,
-                                                          product_option_id = product_option_id
-                                                        )
-            if not is_created:
+            if not is_order_created:
                 cart.quantity += quantity
 
         except JSONDecodeError:
