06f152363222a6438c4a24a28126f4ea71f0e010
taekhyang <xorgid1195@gmail.com>
2021-03-22 11:16:08 +0900

    MODIFY : modified post()

--- order/views.py
+++ order/views.py
@@ -1,6 +1,14 @@
 from django.views import View
 
 
+PRODUCT_OPTION_ID = 1
+
+PRODUCT_ID = 5
+
+
 class CartView(View):
     def post(self, request):
-        pass
+        product_id        = PRODUCT_ID
+        product_option_id = PRODUCT_OPTION_ID
+        
+        
