898a456f338d3372fda4bb33cab3d4c3e42a1293
taekhyang <xorgid1195@gmail.com>
2021-03-23 18:32:41 +0900

    ADD : added order view

--- order/views.py
+++ order/views.py
@@ -15,11 +15,11 @@ class CartView(View):
     @auth_check
     def post(self, request):
         try:
-            result     = json.loads(request.body)['results']
+            results    = json.loads(request.body)['results']
             product_id = results[0]['product_id']
 
             before_purchase, _ = OrderStatus.objects.get_or_create(id=1, status='구매전')
-            order, _           = Order.objects.get_or_create(user=request.user, order_status=before_purchase)
+            order, _           = Order.objects.get_or_create(user=reqeust.user, order_status=before_purchase)
             product            = Product.objects.get(id=product_id)
             
             for result in results:
@@ -35,7 +35,7 @@ class CartView(View):
                                                                   defaults          = {'quantity': product_option_quantity}
                                                                 )
                     if not is_created:
-                        cart.quantity += option['product_option_quantity']
+                        cart.quantity += product_option_quantity
                         cart.save()
                 else:
                     cart, is_created = Cart.objects.get_or_create(
@@ -46,7 +46,7 @@ class CartView(View):
                     if not is_created:
                         cart.quantity += quantity
                         cart.save()
-                return JsonResponse({'message': 'SUCCESS'}, status=201)
+            return JsonResponse({'message': 'SUCCESS'}, status=201)
 
         except JSONDecodeError:
             return JsonResponse({'message': 'JSON_DECODE_ERROR'}, status=400)
@@ -61,19 +61,23 @@ class CartView(View):
             order = Order.objects.get(user=request.user, order_status_id=1)
 
             carts     = order.cart_set.all()
-            cart_list = [{'product_id': cart.product.id,
-                          'product_thumbnail': cart.product.thumbnail_image_url,
-                          'product_name': cart.product.name,
-                          'quantity': cart.quantity,
-                          'product_price': float(cart.product.price) if not cart.product_option else float(cart.product.price + cart.product_option.additional_price),
-                          'total_price': cart.quantity * float(cart.product.price) if not cart.product_option else cart.quantity * float(cart.product.price + cart.product_option.additional_price),
-                          'product_option_id': cart.product_option_id,
-                          'product_option_classification': cart.product_option.option.classification,
-                          'product_option_name': cart.product_option.option.name} for cart in carts]
+            cart_list = [
+                         {'product_id'                   : cart.product.id,
+                          'product_thumbnail'            : cart.product.thumbnail_image_url,
+                          'product_name'                 : cart.product.name,
+                          'quantity'                     : cart.quantity,
+                          'product_price'                : float(cart.product.price) if not cart.product_option \
+                                                           else float(cart.product.price + cart.product_option.additional_price),
+                          'total_price'                  : cart.quantity * float(cart.product.price) if not cart.product_option \
+                                                           else cart.quantity * float(cart.product.price + cart.product_option.additional_price),
+                          'product_option_id'            : cart.product_option.id if cart.product_option else '',
+                          'product_option_classification': cart.product_option.option.classification if cart.product_option else '',
+                          'product_option_name'          : cart.product_option.option.name if cart.product_option else ''} for cart in carts]
             return JsonResponse({'results': cart_list}, status=200)
 
         except JSONDecodeError:
             return JsonResponse({'message': 'JSON_DECODE_ERROR'}, status=400)
         except KeyError:
             return JsonResponse({'message': 'KEY_ERROR'}, status=400)
-        
+
+    
