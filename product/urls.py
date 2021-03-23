a5014b64e950e6a1aadcb99364c7fcba92e1976d
taekhyang <xorgid1195@gmail.com>
2021-03-23 12:53:53 +0900

    MODIFY : modified url path

--- product/urls.py
+++ product/urls.py
@@ -1,7 +1,6 @@
 from django.urls      import path
-from django.conf.urls import re_path
 from .views           import CategoryView
 
 urlpatterns = [
-    re_path(r'^/category/(?P<category_name>[ㄱ-ㅎ가-힣]+)$', CategoryView.as_view()),
+    path(r'/category/<str:category_name>', CategoryView.as_view()),
 ]
